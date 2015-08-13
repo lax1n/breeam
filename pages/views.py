90c3374414299a724ae3ef00ec99ffbe0b0df0c3
lax1n <august.haug@gmail.com>
2015-08-13 15:25:29 +0200

    Button color classes addded in html

--- pages/views.py
+++ pages/views.py
@@ -12,10 +12,13 @@ def index(request):
 
 def show_slide(request, slug):
     slide = get_object_or_404(Slide, slug=slug)
+    button_styles = ['button-style-blue', 'button-style-green', 'button-style-red', 'button-style-purple',
+                     'button-style-orange', 'button-style-yellow']
     return render_to_response("slide.html",
                               {'slide': slide,
                                'slide_prev': slide.get_previous_sibling,
                                'slide_next': slide.get_next_sibling,
                                'siblings': slide.get_siblings(include_self=True),
-                               'buttons': slide.slidebutton_set.all},
+                               'buttons': slide.slidebutton_set.all,
+                               'button_styles': button_styles},
                               context_instance=RequestContext(request))
