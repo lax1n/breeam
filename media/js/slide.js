ea90f8c86c3615600eff281178b99d3c376bfbd9
AtleNO1 <atlekoll@gmail.com>
2015-08-15 15:11:18 +0200

    big update

--- media/js/slide.js
+++ media/js/slide.js
@@ -1,10 +1,51 @@
-$(document).ready(function(){
-    var first = $('div#timeline a:first-child').attr('href');
-    $('#content').load('/pages/' + first);
 
-   $('div#timeline a').click(function(){
-      var page = $(this).attr('href');
-       $('#content').load('/pages/' + page);
-       return false;
-   });
+$(function(){
+	
+	var content;
+	
+	var init = function(){
+		setHTMLObjects();
+		setStartUp();
+		setEvents();
+		
+		function setHTMLObjects(){
+			content = $("#content");
+		};
+		
+		function setStartUp(){
+			var first = $('div#timeline a:first-child').attr('href');
+   			content.load('/pages/' + first);
+		};
+		
+		function setEvents(){
+			$('div#timeline a').click(newPageClickEvent);
+			$(".pagination a").click(function(e){
+				e.preventDefault();
+				// Remove the selected class from the currently selected indicator
+				$(this).parent().parent().find(".selected").removeClass("selected");
+				// Make the clicked indicator the selected one
+				$(this).addClass("selected");
+			});
+		};
+	}();
+	
+	function newPageClickEvent(){
+	   var toLoad = '/pages/' + $(this).attr('href');
+	   content.animate({
+				left: "100%",
+				opacity: 0
+		   }, 1000, loadContent);
+		   
+       function loadContent() {
+        	content.load(toLoad,'',showNewContent())
+    	}
+		
+    	function showNewContent() {
+        	content.animate({
+				left: "0%",
+				opacity: 1
+		   }, 1000);;
+    	}
+    	return false;
+	};	
 });
