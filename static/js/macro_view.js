0871f3ca57a6cdc09e08f9fb8f952ebd0ea90db1
AtleNO1 <atlekoll@gmail.com>
2015-08-27 00:51:21 +0200

    updates

---
+++ static/js/macro_view.js
@@ -0,0 +1,19 @@
+
+$(function(){
+	
+	//globale variabler er plassert her
+	
+	var init = function(){
+		setHTMLObjects();
+		setEvents();
+		
+		function HTMLObjects(){
+			
+		};
+		
+		function setEvents(){
+			
+		};
+	}();
+	
+});
