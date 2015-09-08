3806b9a144b6b041f50378c83a6bd0a82f496711
lax1n <august.haug@gmail.com>
2015-09-08 12:55:44 +0200

    Jobber med timeline systemet

--- static/js/timeline.js
+++ static/js/timeline.js
@@ -1,8 +1,11 @@
 $(function(){
 
-    var timeline_size = timelineSize;
-    var start = timelineSize - 1;
-    var end = timelineSize*2 - 1;
+    var startDefault = 1;
+    var endDefault = timelineSize;
+    var start = 0;
+    var end = 0;
+    var prevStart = startDefault;
+    var prevEnd = endDefault;
     var timeline;
 	
 	var init = function(){
@@ -20,10 +23,85 @@ $(function(){
 		function setEvents(){
             $(document).on('click', "div#timeline a:last-child", function (e) {
                 e.preventDefault();
-                nextTimeline();
+                processTimelineVariables('next');
+            });
+            $(document).on('click', "div#timeline a:first-child", function (e) {
+                e.preventDefault();
+                processTimelineVariables('prev');
             });
 		};
 	}();
+
+    function processTimelineVariables(direction){
+        var proceed = false;
+        if(direction == 'prev'){
+            //Check if lower than default values
+            if(startDefault >= start){
+                start = startDefault;
+                end = endDefault;
+            }else{
+                //Update to last timeline values
+                start = prevStart;
+                end = prevEnd;
+            }
+        }else if(direction == 'next'){
+            //Check if first time
+            if(startDefault == prevStart){
+                start = timelineSize - 2;
+                end = timelineSize*2 - 2;
+                proceed = true;
+            }else{
+                prevStart = start;
+                prevEnd = end;
+                //Increase timeline values by interval
+                start += timelineSize;
+                end += timelineSize;
+                proceed = true;
+            }
+
+        }else{
+            return false;
+        }
+        if(proceed)
+            updateTimeline(direction);
+    }
+
+    function updateTimeline(direction){
+        var data = getTimelineData();
+        if(data != null){
+            //Load new elements
+            timeline.fadeOut('slow', function () {
+                timeline.html(data);
+                if(direction == 'prev'){
+                    $('div#timeline a:nth-last-child(2)').addClass('selected');
+                }else if(direction == 'next'){
+                    $('div#timeline a:nth-child(2)').addClass('selected');
+                }
+                timeline.fadeIn('slow', function () {
+                    //Update tooltips
+                    $('.tooltip').tooltipster();
+
+                    //Update arrows
+                });
+            });
+        }
+    }
+
+    function getTimelineData(){
+        var timelineData = null;
+        var timelineUrl = '/timeline/' + start + '/' + end;
+        $.ajax({
+            url: timelineUrl,
+            type: 'get',
+            dataType: 'html',
+            async: false,
+            success: function(data){
+                if($(data).find('.pagination__dot').length > 2)
+                    timelineData = data;
+            }
+        });
+        return timelineData;
+    }
 
     function nextTimeline(){
         $.get('/timeline/' + start + '/' + end, function(data){
@@ -34,7 +112,7 @@ $(function(){
                     /*timeline.load('/timeline/' + start + '/' + end, function(){
                     });*/
                     timeline.html($(data));
-                    $('div#timeline a:first-child').addClass('selected');
+                    $('div#timeline a:nth-child(2)').addClass('selected');
                     timeline.fadeIn('slow', function () {
                         //Update tooltips
                         $('.tooltip').tooltipster();
@@ -43,10 +121,27 @@ $(function(){
                     });
                 });
                 //Update timeline variables
-                start += timelineSize;
-                end += timelineSize;
             }
         });
+    }
+    function prevTimeline(){
+        $.get('/timeline/' + start + '/' + end, function(data){
+            //Check if more timeline elements
+            if($(data).find('.pagination__dot').length > 1){
+                //Load new elements
+                timeline.fadeOut('slow', function () {
+                    /*timeline.load('/timeline/' + start + '/' + end, function(){
+                    });*/
+                    timeline.html($(data));
+                    $('div#timeline a:nth-last-child(2)').addClass('selected');
+                    timeline.fadeIn('slow', function () {
+                        //Update tooltips
+                        $('.tooltip').tooltipster();
 
+                        //Update arrows
+                    });
+                });
+            }
+        });
     }
 });
